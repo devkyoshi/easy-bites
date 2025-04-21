3750ba1120c837b3245da43b2f7f4ca1fb3e007d
Yashodha Jayasinghe <yashodhalasithjayasinghe@gmail.com>
2025-04-21 17:24:21 +0530

    Created the delivery service

--- backend/api-gateway/src/main/java/com/ds/apigateway/config/GatewayConfig.java
+++ backend/api-gateway/src/main/java/com/ds/apigateway/config/GatewayConfig.java
@@ -16,6 +16,8 @@ public class GatewayConfig {
                         .uri("http://localhost:8082/"))*/
                 .route("restaurant-service", r -> r.path("/api/restaurant/**")
                         .uri("http://localhost:8082/"))
+                .route("delivery-service", r -> r.path("/api/delivery/**")
+                        .uri("http://localhost:8084/"))
                 .build();
     }
 }
